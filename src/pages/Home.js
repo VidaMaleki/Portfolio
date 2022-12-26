58e2286f191bbd6c37e5c43ce9b380b219233240
Vida Maleki <vida.basmani@gmail.com>
2022-12-25 21:28:44 -0800

    add files

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,6 @@
 import React from "react";
 import '../App.css';
 import './Home.css';
-import backgroundVideo from '../videos/backgroundVideo.mp4';
 import Footer from '../components/Footer'
 
 function Home() {
@@ -9,7 +8,6 @@ function Home() {
     <>
       <div className="home">
         <div className='hero-container'>
-        <video src={backgroundVideo} autoPlay loop muted />
         <h1>All in One Calculator</h1>
         </div>
       </div>
