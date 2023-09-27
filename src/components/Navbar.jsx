f0d50328399905ea98e4a876df5e574f99eeea8c
Vida Maleki <vida.basmani@gmail.com>
2023-09-27 12:25:50 -0700

    Fixed portfolio for mobile version

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,76 +1,87 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
+import * as GiHamburgerMenu from "react-icons/gi";
 
 function Navbar() {
-    const [click, setClick] = useState(false);
-    const [button, setButton] = useState(true);
+  const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
 
-    const handleClick = () => setClick(!click);
-    const closeMobileMenu = () => setClick(false);
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
 
-    const showButton = () => {
-        if (window.innerWidth <= 460) {
-        setButton(false);
-        } else {
-        setButton(true);
-        }
-    };
+  const showButton = () => {
+    if (window.innerWidth <= 460) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
 
-    useEffect(() => {
-        showButton();
-    }, []);
+  useEffect(() => {
+    showButton();
+  }, []);
 
-    window.addEventListener('resize', showButton);
-    return (
-        <>
-        <nav className='navbar'>
-        <div className='navbar-container'>
-          <div className='menu-icon' onClick={handleClick}>
-            <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+  window.addEventListener("resize", showButton);
+  return (
+    <>
+      <nav className="navbar">
+        <div className="navbar-container">
+        <div className="menu-icon" onClick={handleClick}>
+            {click ? (
+              <i className="fas fa-times" />
+            ) : (
+              <GiHamburgerMenu.GiHamburgerMenu />
+            )}
           </div>
-          <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-            <li className='nav-item'>
-              <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+          <div
+            className={`close-icon ${click ? "active" : ""}`}
+            onClick={closeMobileMenu}
+          >
+            <i className="fas fa-times" />
+          </div>
+          <ul className={click ? "nav-menu active" : "nav-menu"}>
+            <li className="nav-item">
+              <Link to="/" className="nav-links" onClick={closeMobileMenu}>
                 Home
               </Link>
             </li>
-            <li className='nav-item'>
+            <li className="nav-item">
               <Link
-                to='/projects'
-                className='nav-links'
+                to="/projects"
+                className="nav-links"
                 onClick={closeMobileMenu}
               >
                 Projects
               </Link>
             </li>
-            <li className='nav-item'>
+            <li className="nav-item">
               <Link
-                to='/education'
-                className='nav-links'
+                to="/education"
+                className="nav-links"
                 onClick={closeMobileMenu}
               >
                 Education
               </Link>
             </li>
-            <li className='nav-item'>
+            <li className="nav-item">
               <Link
-                to='/experience'
-                className='nav-links'
+                to="/experience"
+                className="nav-links"
                 onClick={closeMobileMenu}
               >
                 Experience
               </Link>
             </li>
           </ul>
-          {button }
-          <div className='btn-resume'>
-          <a href="https://docdro.id/F5vvA30" >RESUME</a>
+          {button}
+          <div className="btn-resume">
+            <a href="https://docdro.id/F5vvA30">RESUME</a>
           </div>
         </div>
       </nav>
     </>
-    );
+  );
 }
 
 export default Navbar;
